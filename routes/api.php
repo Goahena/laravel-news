17bcfc63005329117d842fe0ac9d10746fd676e3
ThanhNB-NBT <nguyenthanh120223@gmail.com>
2024-11-20 13:59:01 +0700

    api/contacts

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\Postcontroller;
 use App\Http\Controllers\API\Commentcontroller;
+use App\Http\Controllers\API\ContactController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,3 +17,4 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('posts', PostController::class);
 Route::apiResource('posts.comments', CommentController::class);
+Route::apiResource('contacts', ContactController::class);
