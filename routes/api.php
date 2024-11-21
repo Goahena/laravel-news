af918bb9c9e130379e660602b15f34fc30573e4e
Goahena <trunghau190303@gmail.com>
2024-11-21 10:49:06 +0700

    Save changes in main before merging authanduser

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\API\Commentcontroller;
-<<<<<<< HEAD
 use App\Http\Controllers\API\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-=======
 use App\Http\Controllers\API\CategoryController;
-
->>>>>>> origin/APIbaiviet
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -21,8 +17,5 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('posts', PostController::class);
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('posts.comments', CommentController::class);
-<<<<<<< HEAD
 Route::apiResource('contacts', ContactController::class);
-=======
 
->>>>>>> origin/APIbaiviet
