c650e17c298be976b82aaa4e74feba28a99ddfcf
lequanghoa2603 <lequanghoa2603@gmail.com>
2024-11-21 00:50:41 +0000

    API posts

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class Post extends Model
+{
+    use HasFactory;
+    public $timestamps = true;
+    protected $fillable = [
+        'slug', 'tag', 'description', 'title', 'created_at', 'update_at', 'author_id', 
+        'is_active', 'timer', 'image', 'is_comment'
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'categoryPost', 'postId', 'categoryId');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,16 @@
 <?php
 
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\API\Postcontroller;
+use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\API\Commentcontroller;
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\API\CategoryController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
 Route::apiResource('posts', PostController::class);
+Route::apiResource('categories', CategoryController::class);
 Route::apiResource('posts.comments', CommentController::class);
+
