e40064508fdaba2873d7ea90a297bafd26bc76ad
Mạnh <manhtran18001008@gmail.com>
2024-11-21 22:26:10 +0700

    lấy lại mật khẩu bằng mã 6 số

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
@@ -25,30 +24,12 @@ class Post extends Model
     {
         return $this->belongsToMany(Category::class, 'categoryPost', 'postId', 'categoryId');
     }
-}
-=======
-use Laravel\Scout\Searchable;
-
-class Post extends Model
-{
-    use Searchable;
-    use HasFactory;
-
-    protected $fillable = [
-        'title',
-        'slug',
-        'body',
-        'published_at',
-        'category_id',
-    ];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }   
     public function searchableAs()
     {
         return 'posts_index';
     }
 }
->>>>>>> origin/authanduser
+
+use Laravel\Scout\Searchable;
+
+
